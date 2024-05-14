2f3d8c92dcdf6b063070b5ac7b7d341f4380cc39
Anika <anika@Anikas-MacBook-Pro.local>
2024-05-13 18:28:11 -0700

    Added CoreMotion to detect phone flips

--- CharadesApp/CharadesViewController.swift
+++ CharadesApp/CharadesViewController.swift
@@ -6,25 +6,75 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class CharadesViewController: UIViewController {
 
     @IBOutlet weak var AnimalLabel: UILabel!
     
     @IBOutlet weak var timerLabel: UILabel!
+    let motionManager = CMMotionManager()
     
     let words = ["Frog","Bird","Cat","Snake","Bunny","Lion","Monkey","Shark","Unicorn","Elephant"]
+    var wordIndex = 0
+    var isDebouncing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        AnimalLabel.text = words[0]
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(runTimer), userInfo: nil, repeats: true)
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
+            if data != nil {
+                self.handleTilt(data!)
+            }
+        }
+        showNextWord()
+//   Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(runTimer), userInfo: nil, repeats: true)
+    
+    
+    }
+    
+    func handleTilt(_ data: CMAccelerometerData) {
+        if isDebouncing == true {
+            return
+        }
+        let acceleration = data.acceleration
+        let currentPosition = acceleration.z
+        
+        let upTilt = -0.8
+        let downTilt = 0.8
+        
+        if currentPosition < upTilt{
+//            tilt upward, skip
+            print("Skip")
+            waitForBounceBack()
+            showNextWord()
+        }
+        if currentPosition > downTilt{
+//            tile downward, pass
+            print("Pass")
+            waitForBounceBack()
+            showNextWord()
+        }
+        
+    }
+    
+    func showNextWord() {
+        if wordIndex >= words.count{
+            return
+        } else {
+            AnimalLabel.text = words [wordIndex]
+            wordIndex += 1
+        }
     }
 
-    @objc func runTimer(){
-        print("a second later")
+    func waitForBounceBack() {
+        isDebouncing = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.isDebouncing = false
+        }
     }
-
 }
 
 
